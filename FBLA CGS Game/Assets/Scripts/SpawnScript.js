229ef49fa7358ec90d6868918bbc6e64d429ab40
ethanyim1998 <ethanyim1998@gmail.com>
2017-01-16 13:07:46 -0800

    Scene 2 Update
    Barrel is now a prefab. I added code to make the barrel instantiate
    every 4 to 6 seconds (randomly).

---
+++ FBLA CGS Game/Assets/Scripts/SpawnScript.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+
+//public var turretList : GameObject[] = new GameObject[88];
+public var originalBarrel: GameObject;
+public var barrel : GameObject;
+private var originalBarrelPosition: Vector3;
+private var nextFire : float;
+private var rotation1 : Quaternion;
+public var bScript : BulletScript;
+
+
+function Start(){
+
+	nextFire = 3.0;
+
+	rotation1 = Quaternion.identity;
+	rotation1.eulerAngles = Vector3(0, 0, 0);
+
+	originalBarrelPosition = originalBarrel.transform.position;
+}
+
+
+function Update () {
+	createBullet();
+}
+
+
+function createBullet(){
+	if(Time.timeSinceLevelLoad > nextFire){
+		nextFire = Time.timeSinceLevelLoad + Random.Range(3, 6);
+
+		//Part below for creating barrels
+		var cBullet1 = Instantiate(barrel, originalBarrelPosition, rotation1); //Back wall
+
+	}
+}
