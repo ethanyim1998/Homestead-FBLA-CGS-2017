b99ea5cfe6890fe444640ffdef66c8dea85bfaf6
Eric-C0 <eric.bc8@gmail.com>
2017-01-21 23:38:13 -0800

    Continued testing

---
+++ FBLA CGS Game/Assets/C# Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 50f;
+	public float jumpPower = 150f;
+	public bool grounded;
+
+	private RigidBody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		
+		rb = gameObject.GetComponent<RigidBody2D>();
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
