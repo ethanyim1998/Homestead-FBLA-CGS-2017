ec8371471c766c3b79f46c08f6bc136177ec339f
Eric-C0 <eric.bc8@gmail.com>
2017-01-23 23:33:38 -0800

    Dunno

---
+++ FBLA CGS Game/Assets/C# Scripts/GroundControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundControl : MonoBehaviour {
+	private Player player;
+
+	void Start()
+	{
+		gameObject.GetComponentInParent<Player>();
+	}
+
+	/// <summary>
+	/// Sent when another object enters a trigger collider attached to this
+	/// object (2D physics only).
+	/// </summary>
+	/// <param name="other">The other Collider2D involved in this collision.</param>
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		player.grounded = true;
+	}
+
+	void /// <summary>
+	/// Sent when another object leaves a trigger collider attached to
+	/// this object (2D physics only).
+	/// </summary>
+	/// <param name="other">The other Collider2D involved in this collision.</param>
+	OnTriggerExit2D(Collider2D other)
+	{
+		player.grounded = false;
+	}
+	
+}
--- FBLA CGS Game/Assets/C# Scripts/Player.cs
+++ FBLA CGS Game/Assets/C# Scripts/Player.cs
@@ -5,20 +5,46 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
 	public float speed = 50f;
+	public float maxSpeed = 4;
 	public float jumpPower = 150f;
 	public bool grounded;
 
 	private Rigidbody2D rb;
+	//private Animator anim;
 
 	// Use this for initialization
 	void Start () {
 		
 		rb = gameObject.GetComponent<Rigidbody2D>();
+		//anim = gameObject.GetComponent<Animator>();
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		//anim.SetBool("Grounded", grounded);
+		//anim.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+
+		if (Input.GetAxis("Horizontal") < -0.1f)
+			transform.localScale = new Vector3(-1, 1, 1);
+
+		if (Input.GetAxis("Horizontal") > 0.1f)
+			transform.localScale = new Vector3(1, 1, 1);
+	}
+
+	void /// <summary>
+	/// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	FixedUpdate()
+	{
+		float horiz = Input.GetAxis("Horizontal");
+
+		rb.AddForce(Vector2.right * horiz * speed);
+		if (rb.velocity.x >= maxSpeed) {
+			rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
+		}
+		else if (rb.velocity.x <= -maxSpeed) {
+			rb.velocity = new Vector2(-maxSpeed, rb.velocity.y);
+		}
 	}
 }
