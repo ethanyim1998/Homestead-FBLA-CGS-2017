ee7aae29b8d3d1b7ce4e619264742f8d73ce0edb
ethanyim1998 <ethanyim1998@gmail.com>
2017-01-28 15:40:07 -0800

    Camera Lock Update
    Camera does not follow the player if the wall becomes visible in the
    tutorial.

--- FBLA CGS Game/Assets/Scripts/CameraController.js
+++ FBLA CGS Game/Assets/Scripts/CameraController.js
@@ -4,6 +4,7 @@ private var playerObject : GameObject;
 private var playerTransform : Transform;
 private var cameraShiftX: float = 6;
 private var cameraShiftY: float = 5;
+private var playerX: double;
 
 function Start () {
 	playerObject = GameObject.FindWithTag("Player");
@@ -11,7 +12,9 @@ function Start () {
 }
 
 function Update () {
-	var playerX = playerTransform.position.x;
+	if (playerTransform.position.x > -210 && playerTransform.position.x < -50)
+		playerX = playerTransform.position.x;
 	var playerY = playerTransform.position.y;
+	Debug.Log(playerX);
 	transform.position = new Vector3(playerX + cameraShiftX, playerY + cameraShiftY, transform.position.z);
 }
