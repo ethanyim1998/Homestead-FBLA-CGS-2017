8dc41ff34b454b265cff54591c2865573ad40cc5
ethanyim1998 <ethanyim1998@gmail.com>
2017-01-29 00:13:14 -0800

    Font Update
    Font update.

--- FBLA CGS Game/Assets/Scripts/Level2Text.js
+++ FBLA CGS Game/Assets/Scripts/Level2Text.js
@@ -4,10 +4,7 @@ import UnityEngine.UI;
 
 public var textBox : Text;
 public var instructions: String[] = ["Level 2: Business Questions",
-									 "Answer the following three questions to advance onto the next level", 
-									 "Go to a question mark to receive a question",
-									 "Choose an answer by pressing A, B, or C on your keyboard",
-									 "Once you correctly answer all three, the flag will appear"];
+									 "Using your keybaord, answer the following three questions to advance onto the next level"];
 public var questionAndAnswer: String[] = ["Which type of business is the hardest to start?" + System.Environment.NewLine + "A) Sole Proprietorship" + System.Environment.NewLine + "B) Partnership" + System.Environment.NewLine + "C) Corporation",
 										  "Situation in which a firm legally cease to exist when an owner dies, quits, or a new owner is added is..." + System.Environment.NewLine + "A) Limited Life" + System.Environment.NewLine + "B) Unlimited Life" + System.Environment.NewLine + "C) Liquidity",
 										  "Philosophy that goverment should not interfere with business activity is..." + System.Environment.NewLine + "A) Laissez-faire" + System.Environment.NewLine + "B) Law of Demand" + System.Environment.NewLine + "C) Lockout" ];
