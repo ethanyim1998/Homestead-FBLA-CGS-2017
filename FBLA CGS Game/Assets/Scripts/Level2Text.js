8109a102e6ff777b3465c65fad42d6cc973754f5
ethanyim1998 <ethanyim1998@gmail.com>
2017-01-28 16:35:07 -0800

    Questions Scene Update
    Questions and answers were created.

--- FBLA CGS Game/Assets/Scripts/Level2Text.js
+++ FBLA CGS Game/Assets/Scripts/Level2Text.js
@@ -8,6 +8,10 @@ public var instructions: String[] = ["Level 2: Business Questions",
 									 "Go to a question mark to receive a question",
 									 "Choose an answer by pressing A, B, C, or D on your keyboard",
 									 "Once you correctly answer all three, the flag will appear"];
+public var questionAndAnswer: String[] = ["Which type of business is the hardest to start? \\n A) Sole Proprietorship \\n B) Partnership \\n C) Corporation",
+										  "Situation in which a firm legally cease to exist when an owner dies, quits, or a new owner is added is... \\n A) Limited Life \\n B) Unlimited Life \\n C) Liquidity",
+										  "Philosophy that goverment should not interfere with business activity is... \\n A) Laissez-faire \\n B) Law of Demand \\n C) Lockout" ];
+public var player: GameObject;
 private var instructionNum : int;
 
 
@@ -22,9 +26,11 @@ function Start()
 
 function TimerStart() : IEnumerator
 {
-	printInstructions();
-	yield WaitForSeconds(6);
-	TimerStart();
+	if (instructionNum < instructions.Length){
+		printInstructions();
+		yield WaitForSeconds(6);
+		TimerStart();
+	}
 }
 
 
