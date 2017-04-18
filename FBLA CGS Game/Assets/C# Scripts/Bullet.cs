5ca501b0d6c7c190b2c290adbb0f1fd72550b430
Eric-C0 <eric.bc8@gmail.com>
2017-04-17 22:32:51 -0700

    Finished up bullets and cleaned up movement
    Bullets now destroy on barrel hit and destroy barrel too.
    Bullets self-destruct on wall hit
    Player movement and jump cleaned up
    Added 0.04f jump delay to account for the groundControl extending a
    little under the player and the downward velocity interfering with jump
    acceleration

--- FBLA CGS Game/Assets/C# Scripts/Bullet.cs
+++ FBLA CGS Game/Assets/C# Scripts/Bullet.cs
@@ -15,8 +15,10 @@ public class Bullet : MonoBehaviour
         rb = this.GetComponent<Rigidbody2D>();
         player = GameObject.FindGameObjectsWithTag("Player")[0];
 
-        if (player.GetComponent<Rigidbody2D>().transform.localScale.x >= 0)
+        if (player.GetComponent<Rigidbody2D>().transform.localScale.x >= 0) {
+            transform.localScale = new Vector3(-1 * transform.localScale.x, 1*transform.localScale.y, 1*transform.localScale.z);
             rb.velocity = new Vector2(bulletSpeed, 0);
+        }
         else
             rb.velocity = new Vector2(-bulletSpeed, 0);
 
@@ -30,5 +32,16 @@ public class Bullet : MonoBehaviour
     void Update()
     {
           
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag.Equals("Barrel")) {
+            print("Barrel Hit");
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        } else if (other.gameObject.tag.Equals("Wall")) {
+            print("wall hit");
+            Destroy(this.gameObject);
+        }
     }
 }
--- FBLA CGS Game/Assets/C# Scripts/GroundControl.cs
+++ FBLA CGS Game/Assets/C# Scripts/GroundControl.cs
@@ -2,44 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GroundControl : MonoBehaviour {
-	private Player player;
+public class GroundControl : MonoBehaviour
+{
+    private Player player;
 
-	void Start()
-	{
-		player = gameObject.GetComponentInParent<Player>();
-	}
+	private float groundTime;
 
-	/// <summary>
-	/// Sent when another object enters a trigger collider attached to this
-	/// object (2D physics only).
-	/// </summary>
-	/// <param name="other">The other Collider2D involved in this collision.</param>
-	void OnTriggerEnter2D(Collider2D other)
-	{
-		if (other.gameObject.tag != "Portal" && other.gameObject.tag != "Question" && other.gameObject.tag != "Wall" && other.gameObject.tag != "Flag" && other.gameObject.tag != "Barrel")
-			player.grounded = true;
-	}	
-	/// <summary>
-	/// Sent each frame where another object is within a trigger collider
-	/// attached to this object (2D physics only).
-	/// </summary>
-	/// <param name="other">The other Collider2D involved in this collision.</param>
-	void OnTriggerStay2D(Collider2D other)
-	{
-		if (other.gameObject.tag != "Portal" && other.gameObject.tag != "Question" && other.gameObject.tag != "Wall" && other.gameObject.tag != "Flag" && other.gameObject.tag != "Barrel")
-			player.grounded = true;
-	}
+	public float delayTime = 0.04f;
+    void Start()
+    {
+        player = gameObject.GetComponentInParent<Player>();
+    }
 
-	void /// <summary>
-	/// Sent when another object leaves a trigger collider attached to
-	/// this object (2D physics only).
-	/// </summary>
-	/// <param name="other">The other Collider2D involved in this collision.</param>
-	OnTriggerExit2D(Collider2D other)
-	{
-		if (other.gameObject.tag != "Portal" && other.gameObject.tag != "Question" && other.gameObject.tag != "Wall" && other.gameObject.tag != "Flag" && other.gameObject.tag != "Barrel")
+    /// <summary>
+    /// Sent when another object enters a trigger collider attached to this
+    /// object (2D physics only).
+    /// </summary>
+    /// <param name="other">The other Collider2D involved in this collision.</param>
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag != "Portal" && other.gameObject.tag != "Question" && other.gameObject.tag != "Wall" && other.gameObject.tag != "Flag" && other.gameObject.tag != "Barrel")
+        {
+            //StartCoroutine(Delay(1f));  Does not work
+			print("Clock started");
+			groundTime = Time.realtimeSinceStartup;
+        }
+    }
+
+    IEnumerator Delay(float delay)
+    {
+        print(Time.timeSinceLevelLoad);
+        yield return new WaitForSeconds(delay);
+        print(Time.timeSinceLevelLoad);
+
+    }
+
+    void /// <summary>
+         /// Sent when another object leaves a trigger collider attached to
+         /// this object (2D physics only).
+         /// </summary>
+         /// <param name="other">The other Collider2D involved in this collision.</param>
+   OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag != "Portal" && other.gameObject.tag != "Question" && other.gameObject.tag != "Wall" && other.gameObject.tag != "Flag" && other.gameObject.tag != "Barrel") {
+			print("left ground");
+			groundTime = 0;
 			player.grounded = false;
+		}
+    }
+
+	void Update() {
+		if (groundTime != 0 && groundTime  + delayTime  <= Time.realtimeSinceStartup) {
+			print("Time passed");
+			groundTime = 0;
+			player.grounded = true;
+		}
 	}
-	
+
 }
