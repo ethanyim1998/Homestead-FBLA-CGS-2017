5363c7518b9afed708caacfbda452fa70bbc7a29
Eric-C0 <eric.bc8@gmail.com>
2017-04-23 21:40:49 -0700

    Updated file

--- FBLA CGS Game/Assets/C# Scripts/GroundControl.cs
+++ FBLA CGS Game/Assets/C# Scripts/GroundControl.cs
@@ -4,73 +4,73 @@ using UnityEngine;
 
 public class GroundControl : MonoBehaviour
 {
-    private string[] invalidList = new string[] { "Portal", "Question", "Wall", "Flag", "Bullet", "Player", "Barrel" };
-    private Player player;
+	private string[] invalidList = new string[] { "Portal", "Question", "Wall", "Flag", "Bullet", "Player", "Barrel" };
+	private Player player;
 
-    private float groundTime;
+	private float groundTime;
 
-    public float delayTime = 0.04f;
-    void Start()
-    {
-        player = gameObject.GetComponentInParent<Player>();
-    }
+	public float delayTime = 0.04f;
+	void Start()
+	{
+		player = gameObject.GetComponentInParent<Player>();
+	}
 
-    /// <summary>
-    /// Sent when another object enters a trigger collider attached to this
-    /// object (2D physics only).
-    /// </summary>
-    /// <param name="other">The other Collider2D involved in this collision.</param>
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (checkForInvalidCollision(other.gameObject.tag))
-        {
-            //StartCoroutine(Delay(1f));  Does not work
-            print("Clock started");
-            groundTime = Time.realtimeSinceStartup;
-        }
-    }
+	/// <summary>
+	/// Sent when another object enters a trigger collider attached to this
+	/// object (2D physics only).
+	/// </summary>
+	/// <param name="other">The other Collider2D involved in this collision.</param>
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (checkForInvalidCollision(other.gameObject.tag))
+		{
+			//StartCoroutine(Delay(1f));  Does not work
+			print("Clock started");
+			groundTime = Time.realtimeSinceStartup;
+		}
+	}
 
-    private bool checkForInvalidCollision(string other)
-    {
-        for (int i = 0; i < invalidList.Length; i++)
-        {
-            if (other == invalidList[i])
-                return false;
-        }
-        return true;
-    }
+	private bool checkForInvalidCollision(string other)
+	{
+		for (int i = 0; i < invalidList.Length; i++)
+		{
+			if (other == invalidList[i])
+				return false;
+		}
+		return true;
+	}
 
-    IEnumerator Delay(float delay)
-    {
-        print(Time.timeSinceLevelLoad);
-        yield return new WaitForSeconds(delay);
-        print(Time.timeSinceLevelLoad);
+	IEnumerator Delay(float delay)
+	{
+		print(Time.timeSinceLevelLoad);
+		yield return new WaitForSeconds(delay);
+		print(Time.timeSinceLevelLoad);
 
-    }
+	}
 
-    void /// <summary>
-         /// Sent when another object leaves a trigger collider attached to
-         /// this object (2D physics only).
-         /// </summary>
-         /// <param name="other">The other Collider2D involved in this collision.</param>
-   OnTriggerExit2D(Collider2D other)
-    {
-        if (other.gameObject.tag != "Portal" && other.gameObject.tag != "Question" && other.gameObject.tag != "Wall" && other.gameObject.tag != "Flag" && other.gameObject.tag != "Barrel")
-        {
-            print("left ground");
-            groundTime = 0;
-            player.grounded = false;
-        }
-    }
+	void /// <summary>
+	/// Sent when another object leaves a trigger collider attached to
+	/// this object (2D physics only).
+	/// </summary>
+	/// <param name="other">The other Collider2D involved in this collision.</param>
+	OnTriggerExit2D(Collider2D other)
+	{
+		if (checkForInvalidCollision(other.gameObject.tag))
+		{
+			print("left ground");
+			groundTime = 0;
+			player.grounded = false;
+		}
+	}
 
-    void Update()
-    {
-        if (groundTime != 0 && groundTime + delayTime <= Time.realtimeSinceStartup)
-        {
-            print("Time passed");
-            groundTime = 0;
-            player.grounded = true;
-        }
-    }
+	void Update()
+	{
+		if (groundTime != 0 && groundTime + delayTime <= Time.realtimeSinceStartup)
+		{
+			print("Time passed");
+			groundTime = 0;
+			player.grounded = true;
+		}
+	}
 
 }
